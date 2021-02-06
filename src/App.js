7ff0cbb27870ce88168cc68088e69515abac794b
cindylwang <cindy.l.wang.24@dartmouth.edu>
2021-02-06 18:24:40 -0500

    updated app and timer files

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
 import './App.css';
 import ReactDOM from 'react';
-import Timer from './components/Timer/Timer';
+import OurTimer from './components/Timer/Timer';
 import Container from 'react-bootstrap/Container';
 import Button from 'react-bootstrap/Button';
-import Jumbotron from 'react-bootstrap/Jumbotron';
 import React, { ComponentType, useState, useRef } from 'react';
 
-
-function App() {   
+  
+function App() {  
+  
   return (
     <div className="App">
-      <Container>
         <header className="App-header">
-          <a>
-          
+          <a> 
             Pomodoer
           </a>
         </header>
-      </Container>
-      <Jumbotron> 
-        <Timer startCount = '1000' />
-      </Jumbotron> 
+        <div>
+          <OurTimer /> 
+        </div>
+        
+
     </div>
   );
 }
--- src/components/Timer/Timer.js
+++ src/components/Timer/Timer.js
@@ -1,8 +1,9 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import './Timer.css'
 import Button from 'react-bootstrap/Button';
 
- class Timer extends Component {
+
+class OurTimer extends Component {
     constructor(props) {
       super(props);
       this.state ={
@@ -15,7 +16,7 @@ import Button from 'react-bootstrap/Button';
       }
     }
   
-    //handling if work/rest or start/stop button is pushed, alters states so we can change state-reliant displays later
+  //   //handling if work/rest or start/stop button is pushed, alters states so we can change state-reliant displays later
     handleClick() {
       this.setState({
         started: (!this.state.started),
@@ -23,51 +24,46 @@ import Button from 'react-bootstrap/Button';
       })
     }
   
-    componentDidMount(){
-      const {startCount} = this.props
-      this.setState({
-          count: startCount
-      })
-      if (this.state.started === true) {
-        this.interval = setInterval(()=>{
-          this.setState(prevState =>({
-              count: prevState.count - 1
-              }))
-          }, 1000)
-      }
+  //   componentDidMount(){
+  //     const {startCount} = this.props
+  //     this.setState({
+  //         count: startCount
+  //     })
+  //     if (this.state.started === true) {
+  //       this.interval = setInterval(()=>{
+  //         this.setState(prevState =>({
+  //             count: prevState.count - 1
+  //             }))
+  //         }, 1000)
+  //     }
 
-  }
+  // }
 
 
-//     const tick = () => {
-//       if (___ || this.state.over === false) return;
-//       if (this.state.minutes === 0 && this.state.seconds === 0) this.setState({over: true});
-//       else if (this.state.seconds === 0) {
-//          this.setState({minutes: this.state.minutes - 1})
-//          this.setState({seconds: 59})
-//       } else {
-//          this.setState({seconds: this.state.seconds - 1})
-//       }
 
-  componentWillUnmount(){
-      clearInterval(this.interval)
-  }
+ 
+
+
+  // componentWillUnmount(){
+  //     clearInterval(this.interval)
+  // }
     
     render () {
       let startStatus = (this.state.started ? 'STOP' : 'START');
       let workStatus = (this.state.work ? 'REST' : 'WORK');
-      const {count} = this.state
+      // const {count} = this.state
 
       return (
         <section className="timer-container">
           <row>
+            <p>hello please work</p>
           {/* hopefully renders buttons in a row on top of the timer. added custom css for the buttons too!*/}
-          <Button variant="secondary" onClick={() => this.handleClick()}>{startStatus}</Button>{' '}
-          <Button variant="secondary" onClick={() => this.handleClick()}>{workStatus}</Button>{' '}
+          <Button variant="secondary" onClick={this.handleClick()}>{startStatus}</Button>{' '}
+          <Button variant="secondary" onClick={this.handleClick()}>{workStatus}</Button>{' '}
           </row>
           <section className="timer">
             <div>
-                <h1>{count}</h1>
+                {/* <h1>{count}</h1> */}
             </div>
           </section>
         </section> 
@@ -75,9 +71,37 @@ import Button from 'react-bootstrap/Button';
     }
     
     
-}
+  }
 
   // exporting component
-  export default Timer
+  export default OurTimer
+
+
+
+
+
+//     const tick = () => {
+//       if (this.state.work === false || this.state.over === false)
+//       if (this.state.minutes === 0 && this.state.seconds === 0) this.setState({over: true});
+//       else if (this.state.rest === true) rest();
+//       else if (this.state.seconds === 0) {
+//          this.setState({minutes: this.state.minutes - 1})
+//          this.setState({seconds: 59})
+//       } else {
+//          this.setState({seconds: this.state.seconds - 1})
+//       }
+
+//     const rest = () => {
+          // if (this.state.work === true) return;
+          // else{
+          //   this.setState({minutes: 4})
+          //   this.setState({seconds: 59})
+          // if (this.state.seconds === 0) {
+            //  this.setState({minutes: this.state.minutes - 1})
+            //  this.setState({seconds: 59})
+            //} else {
+            //  this.setState({seconds: this.state.seconds - 1})
+          // } 
+//      }
   
   
